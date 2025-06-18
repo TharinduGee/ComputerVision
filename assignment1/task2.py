7b53ed77135227742e1b82384b67c1fd9b16ad9f
Tharindu <tharindugeethanjan@gmail.com>
2025-06-18 15:26:27 +0530

    spatial average task

---
+++ assignment1/task2.py
@@ -0,0 +1,33 @@
+import cv2
+import numpy as np
+
+def spatial_average(image):
+    
+     # function to define the custom size kernal to perform spatial average 
+     def average_kernel(size):
+          return np.ones((size, size), dtype=np.float32) / (size * size)
+
+     spatial_avg_3x3 = cv2.filter2D(image, -1, average_kernel(3))
+     spatial_avg_10x10 = cv2.filter2D(image, -1, average_kernel(10))
+     spatial_avg_20x20 = cv2.filter2D(image, -1, average_kernel(20))
+
+     cv2.imshow('Original Image', image)
+     cv2.imshow('3x3 Average', spatial_avg_3x3)
+     cv2.imshow('10x10 Average', spatial_avg_10x10)
+     cv2.imshow('20x20 Average', spatial_avg_20x20)
+
+     cv2.waitKey(0)
+     cv2.destroyAllWindows()
+
+def main():
+     image_path = str(input("Enter the relative path of the image: "))
+     image = cv2.imread(image_path)
+     if image is None:
+        raise FileNotFoundError(f"Image not found at path: {image_path}")
+     
+     spatial_average(image);
+
+if __name__ == "__main__":
+     main()
+
+
